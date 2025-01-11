a904a75eb5ce07ae3a89f014e209a88061f46bf3
shelepuginivan <shelepuginivanm@gmail.com>
2025-01-11 14:16:02 +0300

    feat(config): set default icon paths

--- config/default.go
+++ config/default.go
@@ -1,3 +1,18 @@
 package config
 
-var defaultConfig = Config{}
+import (
+	"path/filepath"
+
+	"github.com/adrg/xdg"
+)
+
+var defaultConfig = Config{
+	Icons: IconsConfig{
+		Hibernate: filepath.Join(xdg.ConfigHome, "powermenu", "icons", "hibernate.svg"),
+		Lock:      filepath.Join(xdg.ConfigHome, "powermenu", "icons", "lock.svg"),
+		Logout:    filepath.Join(xdg.ConfigHome, "powermenu", "icons", "logout.svg"),
+		PowerOff:  filepath.Join(xdg.ConfigHome, "powermenu", "icons", "poweroff.svg"),
+		Reboot:    filepath.Join(xdg.ConfigHome, "powermenu", "icons", "reboot.svg"),
+		Suspend:   filepath.Join(xdg.ConfigHome, "powermenu", "icons", "suspend.svg"),
+	},
+}
