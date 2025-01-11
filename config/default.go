5e10e8767c0b5856bc8df8068892f91303c23881
shelepuginivan <shelepuginivanm@gmail.com>
2025-01-11 16:40:51 +0300

    feat(config): add commands to default config

--- config/default.go
+++ config/default.go
@@ -1,12 +1,33 @@
 package config
 
 import (
+	"os"
 	"path/filepath"
 
 	"github.com/adrg/xdg"
 )
 
 var defaultConfig = Config{
+	Commands: CommandsConfig{
+		Hibernate: Command{
+			Name: "systemctl",
+			Args: []string{"hibernate"},
+		},
+		Logout: Command{
+			Name: "pkill",
+			Args: []string{"-KILL", "-u", os.ExpandEnv("$USER")},
+		},
+		PowerOff: Command{
+			Name: "poweroff",
+		},
+		Reboot: Command{
+			Name: "reboot",
+		},
+		Suspend: Command{
+			Name: "systemctl",
+			Args: []string{"suspend"},
+		},
+	},
 	Icons: IconsConfig{
 		Hibernate: filepath.Join(xdg.ConfigHome, "powermenu", "icons", "hibernate.svg"),
 		Lock:      filepath.Join(xdg.ConfigHome, "powermenu", "icons", "lock.svg"),
