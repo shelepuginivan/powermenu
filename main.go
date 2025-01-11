985aff4f9010c16c9a01948fbe7e2f0d53080375
shelepuginivan <shelepuginivanm@gmail.com>
2025-01-11 17:17:20 +0300

    feat: apply options to the main widget

--- main.go
+++ main.go
@@ -1,56 +1,78 @@
 package main
 
 import (
-	"log"
+	"sort"
 
 	"github.com/dlasky/gotk3-layershell/layershell"
 	"github.com/gotk3/gotk3/gtk"
 	"github.com/shelepuginivan/powermenu/config"
+	"github.com/shelepuginivan/powermenu/icons"
+	"github.com/shelepuginivan/powermenu/must"
 	"github.com/shelepuginivan/powermenu/styles"
+	"github.com/shelepuginivan/powermenu/widget"
 )
 
 func main() {
 	// Initialize GTK without parsing any command line arguments.
 	gtk.Init(nil)
 
-	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-	if err != nil {
-		log.Fatal("Unable to create window:", err)
-	}
-
-	config := config.Resolve()
+	win := must.Must(gtk.WindowNew(gtk.WINDOW_TOPLEVEL))
+	cfg := config.Resolve()
 
 	// Initialize layershell for window.
 	layershell.InitForWindow(win)
 	layershell.SetNamespace(win, "powermenu")
 
+	layershell.SetLayer(win, layershell.LAYER_SHELL_LAYER_OVERLAY)
+
 	// Set anchors based on the resolved configuration.
-	layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_TOP, config.Anchors.Top)
-	layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_LEFT, config.Anchors.Left)
-	layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_RIGHT, config.Anchors.Right)
-	layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_BOTTOM, config.Anchors.Bottom)
+	layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_TOP, cfg.Anchors.Top)
+	layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_LEFT, cfg.Anchors.Left)
+	layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_RIGHT, cfg.Anchors.Right)
+	layershell.SetAnchor(win, layershell.LAYER_SHELL_EDGE_BOTTOM, cfg.Anchors.Bottom)
 
 	// Set margins based on the resolved configuration.
-	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_TOP, config.Margins.Top)
-	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_LEFT, config.Margins.Left)
-	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_RIGHT, config.Margins.Right)
-	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_BOTTOM, config.Margins.Bottom)
-
-	// Configure window.
-	win.SetTitle("powermenu")
-	win.Connect("destroy", gtk.MainQuit)
+	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_TOP, cfg.Margins.Top)
+	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_LEFT, cfg.Margins.Left)
+	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_RIGHT, cfg.Margins.Right)
+	layershell.SetMargin(win, layershell.LAYER_SHELL_EDGE_BOTTOM, cfg.Margins.Bottom)
 
 	// Load application CSS.
 	styles.Load()
 
 	// Create a new label widget to show in the window.
-	l, err := gtk.LabelNew("Hello, gotk3!")
-	if err != nil {
-		log.Fatal("Unable to create label:", err)
+	widget := must.Must(widget.New())
+	widget.OnEscape(gtk.MainQuit)
+
+	// Configure window.
+	win.SetTitle("powermenu")
+	win.Connect("destroy", gtk.MainQuit)
+	win.Connect("key-press-event", widget.OnKeyPress)
+	win.Add(widget)
+
+	options := []*config.Option{
+		cfg.Hibernate,
+		cfg.Lock,
+		cfg.Logout,
+		cfg.PowerOff,
+		cfg.Reboot,
+		cfg.Suspend,
 	}
 
-	// Add the label to the window.
-	win.Add(l)
+	sort.Slice(options, func(i, j int) bool {
+		return options[i].Order < options[j].Order
+	})
+
+	for _, option := range options {
+		if option.Enabled {
+			icons.ExistsOrWrite(option.Icon, option.GetDefaultIcon())
+			img := must.Must(gtk.ImageNewFromFile(option.Icon))
+			widget.AddOption(img, option.Command)
+		}
+	}
+
+	// Allow keyboard input.
+	layershell.SetKeyboardMode(win, layershell.LAYER_SHELL_KEYBOARD_MODE_ON_DEMAND)
 
 	// Set the default window size.
 	win.SetDefaultSize(800, 30)
